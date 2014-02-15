088558befbc17508dbc0fd2ba6e79b8f9402eed3
Nleelava <nleelava@nleelava-VirtualBox.(none)>
2014-02-15 17:49:55 -0500

    added Form

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ group :doc do
 end
 
 gem "twitter-bootstrap-rails"
+gem 'simple_form'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -4,4 +4,20 @@ class PicsController < ApplicationController
 		@pics = Pic.all
 	end
 
+	def new
+		@pic = Pic.new
+	end 
+
+	def create
+		Pic.create( pic_params )
+		redirect_to pics_path
+
+	end	
+
+	private
+
+	def pic_params
+		params.require(:pic).permit(:title, :message, :who)
+	end
+
 end
